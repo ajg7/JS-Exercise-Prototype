87eb9df75bfc0b9ed64c35aed0f86dc80ff40392
ajg7 <ajgebara@gmail.com>
2020-07-22 14:20:41 -0600

    Completed the steps in task 1. Will revisit later, and find out why index.html is saying that there are errors.

--- index.js
+++ index.js
@@ -39,9 +39,26 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.stomach = [];
 }
+
+Person.prototype.eat = food => {
+    if(this.stomach.length <= 10) {
+      return this.stomach.push(food);
+    }
+}
+
+Person.prototype.poop = () => {
+    return this.stomach = [];
+}
+
+Person.prototype.toString = () => {
+  return `${this.name}, ${this.age}`;
+}
+
 
 /*
   TASK 2
@@ -57,8 +74,11 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-
+function Car(model, milesPerGallon) {
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
+  this.tank = 0;
+  this.odometer = 0;
 }
 
 /*
